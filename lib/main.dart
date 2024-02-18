d586a7553cf9a9bb89b3e8e2ed131f00d4c201aa
Alex1.ai <alexanderemmanuel1719@gmail.com>
2024-02-18 21:06:16 +0000

    step 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
-import 'package:bloc/bloc.dart';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,51 +16,115 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cubit Course',
+      title: 'Flutter Demo',
       theme: ThemeData(
-      
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  BlocProvider(
+        create: (_) => PersonsBloc(),
+        child: MyHomePage(title: 'Bloc Home Page'),
+      ),
     );
   }
 }
 
-const names = [
-  'Foo',
-  'Bar',
-  'Baz'
-];
-
-// creating a custom iteration  and getting random 
-extension RandomElement<T> on Iterable<T>{
-  T getRandomElement() =>
-    elementAt( math.Random().nextInt(length));
-
+@immutable
+abstract class LoadAction {
+  const LoadAction();
 }
 
-class NamesCubit extends Cubit<String?>{
+@immutable
+class LoadPersonAction implements LoadAction {
+  final PersonUrl url;
+  const LoadPersonAction({required this.url}) : super();
+}
 
-  NamesCubit(): super(null);
+enum PersonUrl { person1, person2 }
 
-  void pickleRandomName() => emit(
-    names.getRandomElement()
+extension UrlString on PersonUrl {
+  String get urlString {
+    switch (this) {
+      case PersonUrl.person1:
+        // TODO: Handle this case.
+        return "http://127.0.0.1:5500/testingbloc_course/api/persons1.json";
 
-  );
+      case PersonUrl.person2:
+        return "http://127.0.0.1:5500/testingbloc_course/api/persons2.json";
+    }
+  }
+}
 
+// person class
+@immutable
+class Person {
+  final String name;
+  final int age;
+
+  const Person({
+    required this.name,
+    required this.age,
+  });
+
+  Person.fromJson(Map<String, dynamic> json)
+      : name = json["name"] as String,
+        age = json["age"] as int;
+}
+
+Future<Iterable<Person>> getPersons(String url) => HttpClient()
+    .getUrl(Uri.parse(url))
+    .then((req) => req.close())
+    .then((resp) => resp.transform(utf8.decoder).join())
+    .then((str) => json.decode(str) as List<dynamic>)
+    .then((list) => list.map((e) => Person.fromJson(e)));
+
+// State
+@immutable
+class FetchResult {
+  final Iterable<Person> persons;
+  final bool isRetrievedFromCache;
+
+  const FetchResult(
+      {required this.persons, required this.isRetrievedFromCache});
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "FetchResult (isRetrievedFrom Cache = $isRetrievedFromCache, person = $persons)";
+  }
+}
+
+class PersonsBloc extends Bloc<LoadAction, FetchResult?> {
+  final Map<PersonUrl, Iterable<Person>> _cache = {};
+  PersonsBloc() : super(null) {
+    on<LoadPersonAction>((event, emit) async {
+      // todo
+      final url = event.url;
+      if (_cache.containsKey(url)) {
+        // we have the value in the cache
+        final cachedPersons = _cache[url]!;
+        final result = FetchResult(
+          persons: cachedPersons,
+          isRetrievedFromCache: true,
+        );
+
+        emit(result);
+      } else {
+        final persons = await getPersons(url.urlString);
+        _cache[url] = persons;
+        final result =
+            FetchResult(persons: persons, isRetrievedFromCache: false);
+        emit(result);
+      }
+    });
+  }
+}
+
+extension Subscript<T> on Iterable<T> {
+  T? operator [](int index) => length > index ? elementAt(index):null;
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -66,79 +133,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late final NamesCubit nameCubit;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    nameCubit = NamesCubit();
-  }
-  // int _counter = 0;
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    nameCubit.close();
-  }
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //     _counter++;
-  //   });
-  // }
-
   @override
   Widget build(BuildContext context) {
- 
     return Scaffold(
       appBar: AppBar(
-      
         title: Text(widget.title),
       ),
-      body: Center(
-       
-        child: StreamBuilder<String?>(
-          stream: nameCubit.stream,
-          builder: (context, snapshot){
-            final button = TextButton(
-              onPressed: (){
-                nameCubit.pickleRandomName();
-              }, 
-              
-              child: Text("Pick a random name")
-              
-              ); 
-              switch (snapshot.connectionState){
-                
-                case ConnectionState.none:
-                  // TODO: Handle this case.
-                  return button;
-                case ConnectionState.waiting:
-                  // TODO: Handle this case.
-                  return button;
-
-                case ConnectionState.active:
-                  // TODO: Handle this case.
-                    return Column(
-                      children: [
-                        Text(snapshot.data?? ""),
-                        button
-                      ],
-                    );
-                    
-                case ConnectionState.done:
-                    return const SizedBox();
-              }
-
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+          onPressed: (){
+            context.read<PersonsBloc>()
+               .add(LoadPersonAction(url: PersonUrl.person1));
           },
-          )
+           child: Text("Loaed json #1"),
+           ),
+
+          TextButton(
+          onPressed: (){
+            context.read<PersonsBloc>()
+               .add(LoadPersonAction(url: PersonUrl.person2));
+          },
+           child: Text("Loaed json #2"),
+           ),
+            ],
+           ),
+
+           BlocBuilder<PersonsBloc, FetchResult?>(
+            buildWhen: (previousResult, currentResult){
+              return previousResult?.persons != currentResult?.persons;
+            },
+            builder: (context, fetchResult){
+              final persons = fetchResult?.persons;
+              if (persons == null){
+                return const SizedBox();
+              }
+              return Expanded(
+                child: ListView.builder(
+                  itemCount: persons.length,
+                  itemBuilder: (context, index) {
+                    final person = persons[index];
+                    return ListTile(
+                      title:  Text(person!.name),
+                    );
+
+                    
+                  }
+                  ),
+              );
+            }
+            
+            )
+
+        ],
       ),
-       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
